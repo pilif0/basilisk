465d010d367783ea0108754ef52a8832374f1dc6
Filip Smola <smola.filip@hotmail.com>
2018-12-18 18:01:35 +0000

    Add definitions parsing

--- include/basilisk/AST.h
+++ include/basilisk/AST.h
@@ -7,7 +7,12 @@
 #define BASILISK_AST_H
 
 #include <vector>
+#include <string>
+#include <memory>
 
+//TODO review usage of std::move once stable
+//TODO add docs to constructors
+//TODO should invariants (e.g. at least one definition in program) be checked here or in parser?
 //! Abstract Syntax Tree definitions
 namespace basilisk::ast {
     /** \addtogroup AST
@@ -20,11 +25,53 @@ namespace basilisk::ast {
 
     //TODO: extract a base class
 
+    //! Identifiers are strings
+    typedef std::string Identifier;
+
+    class Expression {
+            //TODO implement
+    };
+
+    class Statement {
+            //TODO implement
+    };
+
     /** \class Definition
      * \brief Base class for both function and variable definitions
      */
-    class Definition {
-        //TODO implement
+    class Definition {};
+
+    /** \class FunctionDefinition
+     * \brief Function Definition node
+     *
+     * Function Definition node contains the function identifier, a set of arguments and a statement-block body.
+     */
+    class FunctionDefinition : public Definition {
+        public:
+            //! Function identifier
+            const Identifier identifier;
+            //! Arguments of the function in order of declaration
+            const std::vector<Identifier> arguments;
+            //! Non-empty block of statements acting as the function body
+            const std::vector<Statement> body;
+
+            FunctionDefinition(Identifier id, std::vector<Identifier> args, std::vector<Statement> body)
+                : identifier(std::move(id)), arguments(std::move(args)), body(std::move(body)) {}
+    };
+
+    /** \class VariableDefinition
+     * \brief Variable Definition node
+     *
+     * Variable Definition contains the variable identifier and the value expression.
+     */
+    class VariableDefinition : public Definition {
+        public:
+            //! Variable identifier
+            const Identifier identifier;
+            //! Value expression
+            const Expression value;
+
+            VariableDefinition(Identifier id, Expression val) : identifier(std::move(id)), value(val) {}
     };
 
     /** \class Program
@@ -35,12 +82,11 @@ namespace basilisk::ast {
      */
     class Program {
         public:
-            //! Definitions in this program
-            //TODO in order of definition?
-            const std::vector<Definition> definitions;
+            //! Pointers to definitions in this program in order of definition
+            const std::vector<std::unique_ptr<Definition>> definitions;
 
-            //TODO make variadic
-            explicit Program(std::vector<Definition> defs) : definitions(std::move(defs)) {}
+            //TODO make variadic?
+            explicit Program(std::vector<std::unique_ptr<Definition>> defs) : definitions(std::move(defs)) {}
     };
 
     /**
--- src/Parser.cpp
+++ src/Parser.cpp
@@ -9,27 +9,244 @@
 #include <basilisk/AST.h>
 
 #include <vector>
+#include <sstream>
+#include <memory>
+#include <string>
 
+//TODO improve function docs
+//TODO improve reporting of unexpected tokens (might require a substantial redesign to have sufficient information)
+//TODO expose more functions in interface to enable partial parsing and testing?
 namespace basilisk::parser {
 
     /**
-     * \brief Parse from an input token buffer into an AST
-     *
-     * User `get` function to obtain tokens from an input buffer, parse those into an AST and return that tree.
+     * \brief Parse Function Definition from an input token buffer
      *
      * \param get Function to get the next input token
      * \param peek Function to peek at the next input token
-     * \return Resulting AST
+     * \return Pointer to resulting Function Definition node
      */
-     //TODO is peek required?
+    std::unique_ptr<ast::FunctionDefinition> parse_definition_func(const get_function_t &get, const peek_function_t &peek) {
+        // Function definition -> expecting IDENTIFIER, LPAR, optional identifier list, RPAR, LBRAC, statement block and RBRAC
+
+        // Identifier
+        std::string id;
+        {
+            // Get the token
+            tokens::Token t = get();
+
+            // Check the tag
+            if (t.tag != tokens::tags::identifier) {
+                // Unexpected token
+                //TODO test
+                std::ostringstream message;
+                message << "Unexpected token " << t << " when parsing Function Definition and expecting IDENTIFIER.";
+                throw ParserException(message.str());
+            }
+
+            // Extract content
+            id = t.content;
+        }
+
+        // Left parenthesis
+        {
+            // Get the token
+            tokens::Token t = get();
+
+            // Check the tag
+            if (t.tag != tokens::tags::lpar) {
+                // Unexpected token
+                //TODO test
+                std::ostringstream message;
+                message << "Unexpected token " << t << " when parsing Function Definition and expecting LPAR.";
+                throw ParserException(message.str());
+            }
+        }
+
+        // Arguments
+        //TODO
+        std::vector<ast::Identifier> args;
+
+        // Right parenthesis
+        {
+            // Get the token
+            tokens::Token t = get();
+
+            // Check the tag
+            if (t.tag != tokens::tags::rpar) {
+                // Unexpected token
+                //TODO test
+                std::ostringstream message;
+                message << "Unexpected token " << t << " when parsing Function Definition and expecting RPAR.";
+                throw ParserException(message.str());
+            }
+        }
+
+        // Left bracket
+        {
+            // Get the token
+            tokens::Token t = get();
+
+            // Check the tag
+            if (t.tag != tokens::tags::lbrac) {
+                // Unexpected token
+                //TODO test
+                std::ostringstream message;
+                message << "Unexpected token " << t << " when parsing Function Definition and expecting LBRAC.";
+                throw ParserException(message.str());
+            }
+        }
+
+        // Body
+        //TODO
+        std::vector<ast::Statement> body;
+
+        // Right bracket
+        {
+            // Get the token
+            tokens::Token t = get();
+
+            // Check the tag
+            if (t.tag != tokens::tags::rbrac) {
+                // Unexpected token
+                //TODO test
+                std::ostringstream message;
+                message << "Unexpected token " << t << " when parsing Function Definition and expecting RBRAC.";
+                throw ParserException(message.str());
+            }
+        }
+
+        return std::make_unique<ast::FunctionDefinition>(id, args, body);
+    }
+
+    /**
+     * \brief Parse Variable Definition from an input token buffer
+     *
+     * \param get Function to get the next input token
+     * \param peek Function to peek at the next input token
+     * \return Pointer to resulting Variable Definition node
+     */
+    std::unique_ptr<ast::VariableDefinition> parse_definition_var(const get_function_t &get, const peek_function_t &peek) {
+        // Variable definition -> expecting IDENTIFIER, ASSIGN, value STATEMENT and SEMICOLON
+
+        // Identifier
+        std::string id;
+        {
+            // Get the token
+            tokens::Token t = get();
+
+            // Check the tag
+            if (t.tag != tokens::tags::identifier) {
+                // Unexpected token
+                //TODO test
+                std::ostringstream message;
+                message << "Unexpected token " << t << " when parsing Variable Definition and expecting IDENTIFIER.";
+                throw ParserException(message.str());
+            }
+
+            // Extract content
+            id = t.content;
+        }
+
+        // Assign
+        {
+            // Get the token
+            tokens::Token t = get();
+
+            // Check the tag
+            if (t.tag != tokens::tags::assign) {
+                // Unexpected token
+                //TODO test
+                std::ostringstream message;
+                message << "Unexpected token " << t << " when parsing Variable Definition and expecting ASSIGN.";
+                throw ParserException(message.str());
+            }
+        }
+
+        // Value expression
+        //TODO
+        ast::Expression val;
+
+        // Semicolon
+        {
+            // Get the token
+            tokens::Token t = get();
+
+            // Check the tag
+            if (t.tag != tokens::tags::semicolon) {
+                // Unexpected token
+                //TODO test
+                std::ostringstream message;
+                message << "Unexpected token " << t << " when parsing Variable Definition and expecting SEMICOLON.";
+                throw ParserException(message.str());
+            }
+        }
+
+        return std::make_unique<ast::VariableDefinition>(id, val);
+    }
+
+    /**
+     * \brief Parse Definition from an input token buffer
+     *
+     * \param get Function to get the next input token
+     * \param peek Function to peek at the next input token
+     * \return Pointer to resulting Definition node
+     */
+    std::unique_ptr<ast::Definition> parse_definition(const get_function_t &get, const peek_function_t &peek) {
+        // Definition -> expecting IDENTIFIER followed by LPAR for function definition, ASSIGN for variable definition
+        tokens::Token t = peek();
+        if (t.tag == tokens::tags::lpar) {
+            // Function definition
+            return parse_definition_func(get, peek);
+        } else if (t.tag == tokens::tags::assign) {
+            // Variable definition
+            return parse_definition_var(get, peek);
+        } else {
+            // Unexpected token
+            //TODO test
+            std::ostringstream message;
+            message << "Unexpected token " << t << " when parsing Definition and expecting LPAR or ASSIGN.";
+            throw ParserException(message.str());
+        }
+    }
+
+    /**
+     * \brief Parse Program from an input token buffer
+     *
+     * Use `get` function to obtain tokens from an input buffer, parse those into an AST and return that tree.
+     *
+     * \param get Function to get the next input token
+     * \param peek Function to peek at the next input token
+     * \return Resulting Program node
+     */
     ast::Program parse(const get_function_t &get, const peek_function_t &peek) {
         // Root -> in Program node -> expecting set of variable and function definitions
-        std::vector<ast::Definition> definitions{};
+        std::vector<std::unique_ptr<ast::Definition>> definitions{};
 
-        //TODO gather definitions until error or END
+        // Try to gather definitions until END
+        for (tokens::Token t = peek(); t.tag != tokens::tags::end_of_input; t = peek()) {
+            // All definitions start with an identifier
+            if (t.tag == tokens::tags::identifier) {
+                // Consume definition
+                definitions.push_back(parse_definition(get, peek));
+            } else if (t.tag == tokens::tags::error) {
+                // Lexer error
+                //TODO test
+                std::ostringstream message;
+                message << "Lexer error: " << t.content;
+                throw ParserException(message.str());
+            } else {
+                // Unexpected token
+                //TODO test
+                std::ostringstream message;
+                message << "Unexpected token " << t << " when parsing Program and expecting IDENTIFIER as start of Definition.";
+                throw ParserException(message.str());
+            }
+        }
+
 
         // There has to be at least one definition
         if (definitions.empty()) {
+            //TODO test
             throw ParserException("Program has to have at least one definition.");
         }
 
